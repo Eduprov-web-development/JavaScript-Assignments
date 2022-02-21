0a4c6c9d69720d8a45b481e784012d91508dc76a
thephotogenicbug <navinkumarachari@gmail.com>
2022-02-21 17:00:57 +0530

    JS SwitchCase Assignment

---
+++ switchCase.js
@@ -0,0 +1,7 @@
+/* Assignment 
+  Example : Mobile Buying Suggestion
+  If given saving amount is > 10k Buy Android Mobile
+  Else If given amount is > 60k Buy Iphone Mobile
+  Else If given amount is > 5 & < 10 Buy Basic Mobile
+  Else Print "You cannot afford mobile phone now!"
+*/
